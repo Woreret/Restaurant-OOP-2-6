452caa2edd5806e686274086219fa5c93d9a5940
Yegor Datsiy <mrfrolix@gmail.com>
2026-03-23 17:12:51 +0200

    Архітектура MenuItem: перетворено в абстрактний клас, додано клас Pizza

--- MenuItem.h
+++ MenuItem.h
@@ -10,41 +10,21 @@ private:
 
 public:
     MenuItem(std::string n, double p, std::string c) 
-        : name(std::move(n)), price(p), category(std::move(c)) {
-        std::cout << "MenuItem created: " << name << "\n";
-    }
+        : name(std::move(n)), price(p), category(std::move(c)) {}
 
     MenuItem(std::string n, double p) 
-        : MenuItem(std::move(n), p, "No category") { 
-        std::cout << "MenuItem delegated\n";
-    }
+        : MenuItem(std::move(n), p, "No category") {}
 
-    MenuItem() : MenuItem("Unknown dish", 0.0, "General") {
-        std::cout << "MenuItem default created\n";
-    }
+    MenuItem() : MenuItem("Unknown dish", 0.0, "General") {}
 
-    ~MenuItem() {
-        std::cout << "MenuItem destroyed: " << name << "\n";
-    }
+    virtual ~MenuItem() = default;
+
+    virtual void prepare() = 0;
 
     void display() const {
         std::cout << "Dish: " << name << " | Price: " << price << " | Category: " << category << "\n";
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const MenuItem& item) {
-        os << "Dish: " << item.name << " | Price: " << item.price << " | Category: " << item.category;
-        return os;
-    }
 
-    friend std::istream& operator>>(std::istream& is, MenuItem& item) {
-        std::getline(is >> std::ws, item.name);
-        if (!(is >> item.price)) {
-            is.clear();
-            while (is.get() != '\n' && is.good()) {} 
-            item.price = 0.0;
-        }
-        std::getline(is >> std::ws, item.category);
-        return is;
-    }
 };
 
