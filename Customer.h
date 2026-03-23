950ec1393c75ab634a5fcc662258957d36b5f6a6
Yegor Datsiy <mrfrolix@gmail.com>
2026-03-23 17:12:06 +0200

    Рефакторинг Customer: перехід на автоматичний copy,move  та зайвий вивід

--- Customer.h
+++ Customer.h
@@ -11,26 +11,14 @@ private:
 
 public:
     Customer(std::string name, std::string phone, int points)
-        : fullName(std::move(name)), phoneNumber(std::move(phone)), loyaltyPoints(points) {
-        std::cout << "Customer created: " << fullName << "\n";
-    }
+        : fullName(std::move(name)), phoneNumber(std::move(phone)), loyaltyPoints(points) {}
 
-    Customer(std::string name) : Customer(std::move(name), "Unknown", 10) {
-        std::cout << "Customer delegated: " << fullName << "\n";
-    }
+    Customer(std::string name) : Customer(std::move(name), "Unknown", 10) {}
 
-    Customer() : Customer("Guest", "None", 0) {
-        std::cout << "Customer default created\n";
-    }
+    Customer() : Customer("Guest", "None", 0) {}
 
-    ~Customer() {
-        std::cout << "Customer destroyed: " << fullName << "\n";
-    }
+    ~Customer() = default;
 
-    Customer(const Customer& other) = default;
-    Customer(Customer&& other) = default;
-    Customer& operator=(const Customer& other) = default;
-    Customer& operator=(Customer&& other) = default;
 
     void addLoyaltyPoints(int points) {
         this->loyaltyPoints += points;
@@ -38,10 +26,5 @@ public:
 
     void displayInfo() const {
         std::cout << "Customer: " << fullName << " | Phone: " << phoneNumber << " | Points: " << loyaltyPoints << std::endl;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Customer& c) {
-        os << "Customer: " << c.fullName << " | Phone: " << c.phoneNumber << " | Points: " << c.loyaltyPoints;
-        return os;
     }
 };
